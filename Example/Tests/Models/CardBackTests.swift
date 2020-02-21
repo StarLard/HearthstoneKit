323c400ca4465093caff28db0a8962c45d960acc
Caleb Friden <caleb.cfri@gmail.com>
2020-02-21 00:51:48 -0800

    Improve tests and card model

---
+++ Example/Tests/Models/CardBackTests.swift
@@ -0,0 +1,32 @@
+//
+//  CardBackTests.swift
+//  HearthstoneKit_Tests
+//
+//  Created by Caleb Friden on 2/20/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import HearthstoneKit
+
+final class CardBackTests: HSKitTestCase {
+    func testDecodeCardBackFromValidDataDoesNotThrow() throws {
+        let data = openSampleFile(.cardBack)
+        let cardBack = try JSONDecoder().decode(CardBack.self, from: data)
+        XCTAssertEqual(cardBack.id.rawValue, 155)
+        XCTAssertEqual(cardBack.slug, "155-pizza-stone")
+        XCTAssertEqual(cardBack.sortCategoryID.rawValue, 5)
+        XCTAssertEqual(cardBack.name, "Pizza Stone")
+        XCTAssertEqual(cardBack.text, "For when you want to build a deck with extra cheese.\\n\\nAcquired from winning five games in Ranked Play, February 2019.")
+        XCTAssertEqual(cardBack.image, URL(string: "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/2f831a00ef1f7671b8132b8c1b0cd63c1ebb7ad334d8903d59f6bd4f60469ccd.png")!)
+    }
+    
+    func testDecodeCardBackCategoryFromValidDataDoesNotThrow() throws {
+        let data = openSampleFile(.cardBackCategory)
+        let cardBackSortCategory = try JSONDecoder().decode(CardBack.SortCategory.self, from: data)
+        print(cardBackSortCategory)
+        XCTAssertEqual(cardBackSortCategory.id.rawValue, 1)
+        XCTAssertEqual(cardBackSortCategory.slug, "base")
+        XCTAssertEqual(cardBackSortCategory.name, "Basic")
+    }
+}
--- Source/Models/CardBack.swift
+++ Source/Models/CardBack.swift
@@ -7,43 +7,29 @@
 
 import Foundation
 
-public struct CardBack {
-//    {
-//      "id": 155,
-//      "sortCategory": 5,
-//      "text": {
-//        "de_DE": "Für die einen ist es eine Pizza, für die anderen das stabilste Frühstück der Welt.\\n\\nBelohnung für alle Spieler, die im Februar 2019 im gewerteten Modus fünf Spiele gewonnen haben.",
-//        "en_US": "For when you want to build a deck with extra cheese.\\n\\nAcquired from winning five games in Ranked Play, February 2019.",
-//        "es_ES": "Para cuando creas un mazo con extra de queso.\\n\\nOtorgado por ganar cinco partidas en el juego con rango en febrero de 2019.",
-//        "es_MX": "Para esos momentos en los que quieres preparar un mazo con extra queso.\\n\\nSe obtiene al ganar cinco partidas clasificadas durante febrero de 2019.",
-//        "fr_FR": "Très utile quand vous voulez créer un deck avec une couche de fromage supplémentaire.\\n\\nObtenu en gagnant cinq matchs en Partie classée, saison de février 2019.",
-//        "it_IT": "Non preoccuparti, non l'abbiamo farcito con l'ananas.\\n\\nOttenuto vincendo cinque partite in modalità Classificata nel mese di febbraio 2019.",
-//        "ja_JP": "デッキをチーズたっぷりで仕上げたい時に。\\n\\n2019年2月、ランク戦プレイで5勝することにより獲得。",
-//        "ko_KR": "치즈가 듬뿍 들어있는 덱을 하고 싶을 때 사용합니다.\\n\\n2019년 2월 등급전에서 5승 달성 시 획득",
-//        "pl_PL": "Gdy najdzie cię ochota na talię z_podwójnym serem.\\n\\nRewers przyznany za wygraną w pięciu partiach w_trybie rankingowym w lutym 2019_r.",
-//        "pt_BR": "Para quando você quer montar um deck com queijo em dobro.\\n\\nObtido ao vencer cinco partidas no Jogo Ranqueado, fevereiro de 2019.",
-//        "ru_RU": "На случай, если захочется побольше сыра в вашей новой колоде.\\n\\nНаграда за победу в пяти рейтинговых матчах в феврале 2019 г.",
-//        "th_TH": "สำหรับผู้ที่ยังไม่พอใจกับการ์ดขอบชีสทั้งเด็ค\\n\\nได้รับจากการชนะ 5 เกมในเกมจัดอันดับ\\nกุมภาพันธ์ 2019",
-//        "zh_CN": "想在套牌里加入更多的芝士吗，选这款就对了。\\n\\n在2019年2月的排名对战赛季中赢得5场对战获得该奖励。",
-//        "zh_TW": "當你想用更多的起司來組套牌的時候。\\n\\n在2019年2月賽季中，贏得五場排名模式對戰時取得。"
-//      },
-//      "name": {
-//        "de_DE": "Pizza",
-//        "en_US": "Pizza Stone",
-//        "es_ES": "Piedra de pizza",
-//        "es_MX": "Extra queso",
-//        "fr_FR": "Pierre à pizza",
-//        "it_IT": "Pizzastone",
-//        "ja_JP": "ピザストーン",
-//        "ko_KR": "피자스톤",
-//        "pl_PL": "Pizza na wynos",
-//        "pt_BR": "No Forno",
-//        "ru_RU": "Пиццекамень",
-//        "th_TH": "พิซซ่าสโตน",
-//        "zh_CN": "石炉披萨",
-//        "zh_TW": "披薩石"
-//      },
-//      "image": "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/2f831a00ef1f7671b8132b8c1b0cd63c1ebb7ad334d8903d59f6bd4f60469ccd.png",
-//      "slug": "155-pizza-stone"
-//    }
+/// Represents a card back which is equipable by the player.
+public struct CardBack: Decodable {
+    public let id: BlizzardIdentifier
+    public let sortCategoryID: BlizzardIdentifier
+    public let text: String
+    public let name: String
+    public let image: URL
+    public let slug: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case sortCategoryID = "sortCategory"
+        case text
+        case name
+        case image
+        case slug
+    }
+}
+
+extension CardBack {
+    public struct SortCategory: Decodable {
+        public let id: BlizzardIdentifier
+        public let name: String
+        public let slug: String
+    }
 }
